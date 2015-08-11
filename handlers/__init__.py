4e380a1f23f281dd7d2b27483770c8021c3bff8f
Damnever <dxc_wolf@163.com>
2015-08-11 18:53:01 +0800

    make  more scalable

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,8 +1,23 @@
-import chat
-import user
+# -*- coding: utf-8 -*-
+
+import os
+import importlib
+import pkgutil
 
 
-urls = []
+def load_urls(urls=None, attr='urls'):
+    if urls is None:
+        urls = list()
+    # pkg_name = os.path.dirname(__file__)
+    pkg_path = os.path.abspath(os.path.dirname(__file__))
+    pkg_name = os.path.basename(pkg_path)
 
-urls.extend(chat.urls)
-urls.extend(user.urls)
+    for _, name, ispkg in pkgutil.iter_modules([pkg_path]):
+        if ispkg:
+            continue
+        module = importlib.import_module('.'+name, pkg_name)
+        urls.extend(getattr(module, attr))
+
+    return urls
+
+urls = load_urls()
